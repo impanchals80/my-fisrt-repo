7fddf9db246d5ff06e9a45c77a1ab8265a8097bc
AyushMarvel <120002832+AyushMarvel@users.noreply.github.com>
2023-02-09 11:11:07 +0530

    Created AccountController

---
+++ HelloWorldMvc/Controllers/AccountController.cs
@@ -0,0 +1,16 @@
+﻿using System;
+
+public class Class1
+{
+	public Class1()
+	{
+	}
+    public int Add()
+    {
+
+        int a = Convert.ToInt32(Console.ReadLine());
+
+        int b = Convert.ToInt32(Console.ReadLine());
+        return a + b;
+    }
+}
