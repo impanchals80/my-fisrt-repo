0b6065e4c7093eaa8493107d3d3cbd791b71bbe2
AyushMarvel <120002832+AyushMarvel@users.noreply.github.com>
2023-02-09 10:39:55 +0530

    Created Add method

--- HelloWorldMvc/Controllers/ProductDetailController.cs
+++ HelloWorldMvc/Controllers/ProductDetailController.cs
@@ -5,4 +5,13 @@ public class Class1
 	public Class1()
 	{
 	}
+
+	public int Add()
+	{
+		
+		int a= Convert.ToInt32(Console.ReadLine());
+		 
+	    int b= Convert.ToInt32(Console.ReadLine());
+		return a + b;
+	}
 }
